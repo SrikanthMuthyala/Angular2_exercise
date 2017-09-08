3f2343a6bbd2626d80cffc198706d03c6802ba2b
Kavita Wagadre <kavitawagadre@HYDPCM317836D.local>
2017-09-08 17:47:39 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+
+import { WarningalertComponent } from './warningalert/warningalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessalertComponent,
+    WarningalertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/successalert/successalert.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit} from '@angular/core'
+
+@Component({
+    selector : 'app-suceessalert',
+    templateUrl : './successalert.component.html',
+    styleUrls: ['./successalert.component.css']
+
+})
+
+export class SuccessalertComponent{
+
+}
